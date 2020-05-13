e827ad9b43b345286ca1f140300f5f89da97bdb5
elevatebart <ledouxb@gmail.com>
2020-05-13 18:39:12 -0500

    remove declarations

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const createServer: any;
-declare type PluginOptions = {
-    root: string;
-    app: any;
-    server: any;
-    watcher: any;
-};
-declare const myPlugin: ({ root, app, server, watcher }: PluginOptions) => void;
